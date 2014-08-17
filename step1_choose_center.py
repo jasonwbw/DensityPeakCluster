f078044983cee8a0c73f319546a6b120ee8fd39e
jasonwbw <384444165@163.com>
2014-08-18 00:49:04 +0800

    fix attr error

--- step1_choose_center.py
+++ step1_choose_center.py
@@ -8,8 +8,8 @@ from cluster import *
 def plot(data):
 	logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 	dpcluster = DensityPeakCluster()
-	distances, max_dis, min_dis, max_id, rho = local_density(load_paperdata, data)
-	delta, nneigh = min_distance(max_id, distances, rho)
+	distances, max_dis, min_dis, max_id, rho = dpcluster.local_density(load_paperdata, data)
+	delta, nneigh = min_distance(max_id, max_dis, distances, rho)
 	plot_rho_delta(rho, delta)   #plot to choose the threthold
 
 if __name__ == '__main__':
