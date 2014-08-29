5b2a0fed2aed4d2a9dda2f725c275fc8580f1ff6
jasonwbw <384444165@163.com>
2014-08-29 16:36:43 +0800

    add a use demo for iris data

--- step1_choose_center.py
+++ step1_choose_center.py
@@ -5,13 +5,13 @@ import logging
 from plot import *
 from cluster import *
 
-def plot(data):
+def plot(data, auto_select_dc = False):
 	logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 	dpcluster = DensityPeakCluster()
-	distances, max_dis, min_dis, max_id, rho = dpcluster.local_density(load_paperdata, data)
+	distances, max_dis, min_dis, max_id, rho = dpcluster.local_density(load_paperdata, data, auto_select_dc = auto_select_dc)
 	delta, nneigh = min_distance(max_id, max_dis, distances, rho)
 	plot_rho_delta(rho, delta)   #plot to choose the threthold
 
 if __name__ == '__main__':
-	plot('./data/data_in_paper/example_distances.dat')
-	#plot('./data/data_iris_flower/iris.forcluster')
+	#plot('./data/data_in_paper/example_distances.dat')
+	plot('./data/data_iris_flower/iris.forcluster', auto_select_dc = True)
--- step2_cluster.py
+++ step2_cluster.py
@@ -5,10 +5,10 @@ import logging
 from plot import *
 from cluster import *
 
-def plot(data, density_threshold, distance_threshold):
+def plot(data, density_threshold, distance_threshold, auto_select_dc = False):
 	logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
 	dpcluster = DensityPeakCluster()
-	rho, delta, nneigh = dpcluster.cluster(load_paperdata, data, density_threshold, distance_threshold)
+	rho, delta, nneigh = dpcluster.cluster(load_paperdata, data, density_threshold, distance_threshold, auto_select_dc = auto_select_dc)
 	logger.info(str(len(dpcluster.ccenter)) + ' center as below')
 	for idx, center in dpcluster.ccenter.items():
 		logger.info('%d %f %f' %(idx, rho[center], delta[center]))
@@ -16,5 +16,5 @@ def plot(data, density_threshold, distance_threshold):
 	plot_cluster(dpcluster)
 
 if __name__ == '__main__':
-	plot('./data/data_in_paper/example_distances.dat', 20, 0.1)
-	#plot('./data/data_iris_flower/iris.forcluster', 47.5, 0.9)
+	#plot('./data/data_in_paper/example_distances.dat', 20, 0.1)
+	plot('./data/data_iris_flower/iris.forcluster', 40.7, 0.9, auto_select_dc = True)
